9ebe37549b1aaad80b441297150b6b17b8e053a4
phantomseductio <41136174-phantomseductio@users.noreply.replit.com>
2025-04-20 06:34:02 +0000

    Restored to '140b045e0f4e87a39d0f2ba3e87554a0fc8d7060'
    Replit-Restored-To: 140b045e0f4e87a39d0f2ba3e87554a0fc8d7060

--- workspace/saddlewood-website/.next/server/next-font-manifest.js
+++ workspace/saddlewood-website/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/runner/workspace/workspace/saddlewood-website/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/92f44bb82993d879-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
